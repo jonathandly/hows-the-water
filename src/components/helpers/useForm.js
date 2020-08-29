322ed085bd05cce434a8e517ed480ade57dd150b
Jonathan Daly <dalyj138@gmail.com>
2020-08-29 15:36:17 -0400

    added validation checks for forgot password and change password.

---
+++ src/components/helpers/useForm.js
@@ -0,0 +1,69 @@
+import { useState, useEffect } from 'react';
+
+const useForm = (callback, validate) => {
+	const [values, setValues] = useState({
+		fullname: '',
+		email: '',
+		password: '',
+		mobile: '',
+		regionInput: '',
+		beachInput: '',
+		surferInput: '',
+		oldPassword: '',
+		newPassword: '',
+		confirmPassword: '',
+		imageInput: null,
+		verificationCode: ''
+	});
+
+	const [errors, setErrors] = useState({
+		fullname: '',
+		email: '',
+		password: '',
+		mobile: '',
+		regionInput: '',
+		beachInput: '',
+		surferInput: '',
+		oldPassword: '',
+		newPassword: '',
+		confirmPassword: '',
+		imageInput: null,
+		verificationCode: ''
+	});
+
+	const [isSubmitting, setIsSubmitting] = useState(false);
+
+	const handleChange = event => {
+		event.persist();
+		const { name, value } = event.target;
+		setValues({
+			...values,
+			[name]: value
+		});
+	};
+
+	const handleSubmit = event => {
+		event.preventDefault();
+		console.log('USEFORM:: HANDLE SUBMIT :: SUBMIT CLICKED');
+		// validate form fields
+		setErrors(validate(values));
+		setIsSubmitting(true);
+	};
+
+	useEffect(() => {
+		// call callback if there are no errors
+		if(Object.keys(errors).length === 0 && isSubmitting) {
+			callback();
+		}
+	}, [errors]);
+
+	return {
+		handleChange,
+		handleSubmit,
+		values,
+		errors
+	};
+};
+
+export default useForm;
+
